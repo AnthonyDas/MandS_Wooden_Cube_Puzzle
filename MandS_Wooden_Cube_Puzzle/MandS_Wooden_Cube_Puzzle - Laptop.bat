601ed72fa88de4d01b47790d65f272c2d08c6846
AnthonyDas <anthony.das@gmail.com>
2018-11-09 13:52:23 +0000

    Initial file add.

---
+++ MandS_Wooden_Cube_Puzzle/MandS_Wooden_Cube_Puzzle - Laptop.bat
@@ -0,0 +1,8 @@
+C:
+
+cd C:\Users\AnthonyDas\Documents\Visual Studio 2017\Projects\MandS_Wooden_Cube_Puzzle\MandS_Wooden_Cube_Puzzle
+
+g++ -Wall -Wconversion -std=c++11 -mtune=native -O3 CubeSolver.cpp -o CubeSolver.exe
+
+
+pause
